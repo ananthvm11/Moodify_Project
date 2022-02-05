61d56e9960f5b1d5bce2f13f6056b39e2a21305a
ananthvm11 <ananthvm11@gmail.com>
2022-02-05 12:10:36 +0800

    working with tailwind

--- web-api-auth-examples-master/implicit_grant/public/callback/assets/index.js
+++ web-api-auth-examples-master/implicit_grant/public/callback/assets/index.js
@@ -1,13 +1,12 @@
-  var stateKey = 'spotify_auth_state';  
-  var spotifyUrl = "https://api.spotify.com/v1/me" 
-//   var data = {};
-  let yourGlobalVariable = {};
+var stateKey = 'spotify_auth_state';  
+var spotifyUrl = "https://api.spotify.com/v1/me" 
 
-  /**
-   * Obtains parameters from the hash of the URL
-   * @return Object
-   */
-  function getHashParams() {
+/**
+* Obtains parameters from the hash of the URL
+* @return Object
+*/
+
+function getHashParams() {
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
         q = window.location.hash.substring(1);
@@ -15,102 +14,42 @@
        hashParams[e[1]] = decodeURIComponent(e[2]);
     }
     return hashParams;
-  }
+}
 
-  /**
-   * Generates a random string containing numbers and letters
-   * @param  {number} length The length of the string
-   * @return {string} The generated string
-   */
-  // function generateRandomString(length) {
-  //   var text = '';
-  //   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+var params = getHashParams();
 
-  //   for (var i = 0; i < length; i++) {
-  //     text += possible.charAt(Math.floor(Math.random() * possible.length));
-  //   }
-  //   return text;
-  // };
-
-  // var userProfileSource = document.getElementById('user-profile-template').innerHTML,
-      // userProfileTemplate = Handlebars.compile(userProfileSource),
-      // userProfilePlaceholder = document.getElementById('user-profile');
-
-
-      // oauthSource = document.getElementById('oauth-template').innerHTML,
-      // oauthTemplate = Handlebars.compile(oauthSource),
-      // oauthPlaceholder = document.getElementById('oauth');
-
-  var params = getHashParams();
-
-  var access_token = params.access_token,
-      state = params.state,
-      storedState = localStorage.getItem(stateKey);
+var access_token = params.access_token,
+    state = params.state,
+    storedState = localStorage.getItem(stateKey);
   
   
   if (access_token && (state == null || state !== storedState)) {
     alert('There was an error during the authentication');
   } else {
-    localStorage.removeItem(stateKey);
-    if (access_token) {
-        fetch(spotifyUrl, {
-            method: 'GET',
-            // mode: 'no-cors',
-            // redirect: 'follow',
-            headers:{
-              'Authorization': 'Bearer ' + access_token
-            },
-        })
-        .then(response => response.json())
-        .then(jsonData => {
-            const image = jsonData.images[0].url
-            const imageHtml = document.getElementById("imageSection")
-            imageHtml.innerHTML = 
-            `<div class="avatar">
-                <div class="rounded-full w-10 h-10 m-1">
-                <img src=${image}>
-                </div>
-            </div>`
-        })
-        .catch(error => console.log(error))
+        localStorage.removeItem(stateKey);
+        if (access_token) {
+            fetch(spotifyUrl, {
+                method: 'GET',
+                // mode: 'no-cors',
+                // redirect: 'follow',
+                headers:{
+                'Authorization': 'Bearer ' + access_token
+                },
+            })
+            .then(response => response.json())
+            .then(jsonData => {
+                const image = jsonData.images[0].url
+                const name = jsonData.display_name;
+                console.log(name)
+                console.log(image)
+                const imageHtml = document.getElementById("imageSection")
+                imageHtml.innerHTML = 
+                `<div class="avatar">
+                    <div class="rounded-full w-10 h-10 m-1">
+                    <img src=${image}>
+                    </div>
+                </div>`
+                })
+        }
     }
-
-    //   $.ajax({
-    //       url: 'https://api.spotify.com/v1/me',
-    //       headers: {
-    //         'Authorization': 'Bearer ' + access_token
-    //       },
-    //       success: function testing(response)  {
-    //         // userProfilePlaceholder.innerHTML = userProfileTemplate(response);
-    //         data = response;
-    //         console.log(data.images[0].url)
-    //         // $('#login').hide();
-    //         // $('#loggedin').show();
-    //       }
-    //   });
-    } // else {
-    //     $('#login').show();
-    //     $('#loggedin').hide();
-    // }
-
-    // document.getElementById('login-button').addEventListener('click', function() {
-
-    //   var client_id = 'd824b67943c445e6855abb962d46e0be'; // Your client id
-    //   var redirect_uri = 'http://localhost:8888/callback/index.html'; // Your redirect uri
-      
-    //   var state = generateRandomString(16);
-
-    //   localStorage.setItem(stateKey, state);
-    //   var scope = 'user-read-private user-read-email';
-
-    //   var url = 'https://accounts.spotify.com/authorize';
-    //   url += '?response_type=token';
-    //   url += '&client_id=' + encodeURIComponent(client_id);
-    //   url += '&scope=' + encodeURIComponent(scope);
-    //   url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-    //   url += '&state=' + encodeURIComponent(state);
-    //   window.location = url;
-    // }, false);
-// }
-
   
